d5bdd8342e5f531c4ad46a4f84e10a566d1b56c7
Sasank <thapas@beloit.edu>
2020-11-29 16:37:43 -0600

    Initial Commit... only fun repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Meditation
+
+This is just a fun project I wanted to do try out. Learned about the limits of css and javascript.
+
+Issue: you can't do multiple clicks and hold... that will have a lot of set timeouts which will cause the page to behave weird.
+
+[x] Add Event listener to the body to listen to mouse clicks.
+[x] Add the image of the guy meditating in the middle.
+[x] Create an animation of the guy using transform.
+[x] Add an transparent circle over the guy
---
+++ script.js
@@ -0,0 +1,49 @@
+const allContainer=document.querySelector('.all_container');
+const battery=document.querySelector('.inner_battery');
+let hold=false;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function addAura(){
+    const AuraDiv=document.createElement("DIV");
+    AuraDiv.classList.add("aura");
+    setTimeout(()=>{AuraDiv.remove() },3000)
+    allContainer.appendChild(AuraDiv)
+}
+
+function increaseBattery(){
+    //let curr=window.getComputedStyle(battery).getPropertyValue('width');
+    //console.log(curr)
+    //curr=parseInt(curr)/2
+    //console.log(curr)
+    console.log(battery.style.width);
+    let curr=parseInt(battery.style.width);
+    if(curr==100) return
+    curr+=5;
+    battery.style.width=`${curr}%`;
+}
+
+let delay=3000;
+function addAuraNonStop(){
+    addAura();
+    increaseBattery()
+    setTimeout(()=>{
+        if(hold && this.clickNum==clickNum){
+            addAuraNonStop()
+            if(delay>=1000)
+                delay-=200;
+        }
+    },delay);
+}
+
+document.body.addEventListener('mousedown',(e)=>{
+    delay=2000;
+    hold=true; 
+    addAuraNonStop();
+})
+
+document.body.addEventListener('mouseup',(e)=>{
+    hold=false;
+})
